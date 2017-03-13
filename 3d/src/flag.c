b3108d33c9d0b852421f63bbaeb268eab2100eba
Olivier-Daire <daire.olivier@gmail.com>
2017-03-13 11:01:31 +0100

    Flag grid

--- 3d/src/flag.c
+++ 3d/src/flag.c
@@ -1,36 +1,18 @@
-/*=================================================================*/
-/*= E.Incerti - incerti@upem.fr                                   =*/
-/*= Université Paris-Est-Marne-la-Vallée                          =*/
-/*= Exemple de fonctionalités de lib. graphique <g3x>             =*/
-/*=================================================================*/
-
 #include <g3x.h>
 #include "PMat.h"
 #include "Link.h"
 
-/* des couleurs prédéfinies */  
-static G3Xcolor rouge  ={1.0,0.0,0.0};
-static G3Xcolor jaune  ={1.0,1.0,0.0};
-static G3Xcolor vert   ={0.0,1.0,0.0};
-static G3Xcolor cyan   ={0.0,1.0,1.0};
-static G3Xcolor bleu   ={0.0,0.0,1.0};
-static G3Xcolor magenta={1.0,0.0,1.0};
-/* paramètres géométriques */
-static double angle= 0.00;
-static double rayon= 0.66;
-/* paramètres de lumière/matière */
-static double alpha= 0.5;
-static double ambi = 0.2;
-static double diff = 0.3;
-static double spec = 0.4;
-static double shin = 0.5;
 
 #define MAXCOL 25
 static G3Xcolor colmap[MAXCOL];
-double h = 0.1;
-int nbm = 11;
-int nbl = 2;
+
+#define HEIGHT 24
+#define WIDTH 24
+
+int nbm = HEIGHT * WIDTH;
+int nbl = HEIGHT * WIDTH *4; // FIXME
 Vector3 GRAVITY = {0, -9.1, 0};
+double h;
 
 PMat* TabM;
 Link* TabL;
@@ -41,41 +23,75 @@ PMat* M;
 
 static void Init(void)
 {
-  double Fe = 1 / h;
+  double Fe = 50;
+  h = 1 / Fe;
   double k = 0.7 * Fe*Fe;
-  double z = 0.1 * Fe;
+  double z = 0.001 * Fe;
 
   TabM = (PMat*)calloc(nbm, sizeof(PMat));
-  nbl = (nbm-1);
   TabL = (Link*)calloc(nbl, sizeof(Link));
+
   M = TabM;
+
   Vector3 vel;
   vel.x = 1;
   vel.y = 1;
   vel.z = 1;
 
-  MassFixe(M, (Vector3){-5., -0., 0}); M++;
-  Mass(M, (Vector3) { -4., 0., 0 }, vel, 1); M++;
-  Mass(M, (Vector3) { -3., 0., 0 }, vel, 1); M++;
-  Mass(M, (Vector3) { -2., 0., 0 }, vel, 1); M++;
-  Mass(M, (Vector3) { -1., 0., 0 }, vel, 1); M++;
-  Mass(M, (Vector3) { 0., 0., 0 }, vel, 1); M++;
-  Mass(M, (Vector3) { +1., 0., 0 }, vel, 1); M++;
-  Mass(M, (Vector3) { +2., 0., 0 }, vel, 1); M++;
-  Mass(M, (Vector3) { +3., 0., 0 }, vel, 1); M++;
-  Mass(M, (Vector3) { +4., 0., 0 }, vel, 1); M++;
-  MassFixe(M, (Vector3){+5., 0., 0}); M++;
+//  MassFixe(M, (Vector3){-1., -0., 0}); M++;
+  for (int i = 0; i < nbm ; i++)
+  {
+    if (i < HEIGHT)
+    {
+      Mass(M, (Vector3) { 1 * i , 0., 0 }, vel, 1); M++;
+    } else {
+      Mass(M, (Vector3) { 1 * (i % HEIGHT) , 1 * (i / HEIGHT), 0 }, vel, 1); M++;
+    }
+    
+  }
+  //MassFixe(M, (Vector3){start + 1, 0., 0}); M++;
 
   M = TabM;
   L = TabL;
 
-  while(L < TabL+nbl) {
-    RessortFrein(L, k, z);
-    Connect(M, L, M+1);
-    Gravite(L, GRAVITY);
+  float kMin = 0.25 * k;
+  float step = (1 - kMin) / WIDTH;
+  for (int i = 0; i < nbm - 1; i++) {
+      int col = i / WIDTH;
+      float kOpt =  (1. - col * step) * k;
+
+      //liens verticaux
+      if (((i + 1) % HEIGHT) != 0) { 
+        RessortFrein(L, k, z);
+        Connect(M, L, M+1);
+        L++;
+        //M++;
+      }
+
+    // liens horizontaux
+    if (i < (nbm - HEIGHT)) {
+      RessortFrein(L, kOpt, z);
+      Connect(M, L, M+HEIGHT);
+      L++;
+    }
+
+    // diagonale
+   if (i < (nbm - HEIGHT) && ((i + 1) % HEIGHT) != 0) {
+      RessortFrein(L, kOpt, z);
+      Connect(M, L, M+HEIGHT+1);
+      L++;
+    }
+
+    // diagonale
+    if (i < (nbm - HEIGHT) && ((i) % HEIGHT) != 0) {
+      RessortFrein(L, kOpt, z);
+      Connect(M, L, M+HEIGHT-1);
+      L++;
+    }
+
     M++;
-    L++;
   }
+
 }
 
 void Anim(void)
@@ -83,7 +99,7 @@ void Anim(void)
   M = TabM;
   L = TabL;
   while (M < TabM + nbm) { M->algo(M, h); ++M; }
-  while (L < TabL + nbl) { L->algo(L); ++L; }
+  //while (L < TabL + nbl) { L->algo(L); ++L; }
 }
 
 /*= FONCTION DE DESSIN PRINCIPALE =*/
@@ -92,7 +108,10 @@ static void Dessin(void)
 	M = TabM;
   while (M < TabM + nbm) { M->draw(M); ++M; }
   L = TabL;
-  while (L < TabL + nbl) { L->draw(L); ++L; }
+  for(int i = 0; i < (nbm*4)-142 ; i++) { // FIXME Whyyy ?
+    L->draw(L); L++;
+  }
+  
 }
 
 /*=    ACTION A EXECUTER EN SORTIE   =*/
@@ -102,21 +121,6 @@ static void Exit(void)
 {
   /* rien à faire ici puisqu'il n'y a pas d'allocation dynamique */
 	fprintf(stdout,"\nbye !\n");
-}
-
-/*= action : variation de couleur =*/
-static void action1(void)
-{
-  rouge[0]-=0.01;  rouge[1]+=0.01;
-  vert[1] -=0.01;  vert[2] +=0.01;  
-  bleu[2] -=0.01;  bleu[0] +=0.01;
-}
-/*= action : variation de couleur =*/
-static void action2(void)
-{
-  rouge[0]+=0.01;  rouge[1]-=0.01;
-  vert[1] +=0.01;  vert[2] -=0.01;  
-  bleu[2] +=0.01;  bleu[0] -=0.01;
 }
 
 /*= action : affiche les parametre camera dans le terminal =*/
@@ -136,9 +140,9 @@ int main(int argc, char** argv)
 
 	/* paramètres caméra */
   /* param. géométrique de la caméra. cf. gluLookAt(...) */
-  g3x_SetPerspective(40.,100.,1.);
+  g3x_SetPerspective(90.,100.,1.);
   /* position, orientation de la caméra */
-  g3x_SetCameraSpheric(0.25*PI,+0.25*PI,6.,(G3Xpoint){0.,0.,0.});
+  g3x_SetCameraSpheric(0.5*PI,0,30,(G3Xpoint){20.,0.,0.});
 
   /* fixe les param. colorimétriques du spot lumineux */
 	/* lumiere blanche (c'est les valeurs par defaut)   */	
@@ -151,15 +155,7 @@ int main(int argc, char** argv)
   g3x_SetLightPosition (10.,10.,10.);
   g3x_SetLightDirection( 0., 0., 0.);  
 
-	g3x_SetScrollWidth(4);
-	g3x_CreateScrollh_d("shin",&shin,0.0,1.0,1.0,"intensite lumiere brillance ");
-	g3x_CreateScrollh_d("spec",&spec,0.0,1.0,1.0,"intensite lumiere speculaire");
-	g3x_CreateScrollh_d("diff",&diff,0.0,1.0,1.0,"intensite lumiere diffuse   ");
-	g3x_CreateScrollh_d("ambi",&ambi,0.0,1.0,1.0,"intensite lumiere ambiante  ");
-	
   /* action attachées à des touches */
-  g3x_SetKeyAction('g',action1,"variation de couleur");
-  g3x_SetKeyAction('G',action2,"variation de couleur");
   g3x_SetKeyAction('c',camera_info,"pos./dir. de la camera => terminal");
 
 	/* initialisation d'une carte de couleurs */
